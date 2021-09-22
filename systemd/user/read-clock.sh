3f0bcdc7eb2089fb5bcb92c36fca1bd9d777bea0
TigorHutasuhut <tigor.hutasuhut@gmail.com>
2021-09-22 11:06:53 +0700

    config(systemd): read-clock now uses 24 hour format

--- systemd/user/read-clock.sh
+++ systemd/user/read-clock.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 DISPLAY=:0
-/usr/bin/date +"%I %p" | /usr/bin/awk '{sub(/^0*/,"");}1' | /usr/bin/xargs printf "It's %d %s" | /usr/bin/espeak --stdout | /usr/bin/aplay
+/usr/bin/date +"%H %p" | /usr/bin/awk '{sub(/^0*/,"");}1' | /usr/bin/xargs printf "It's %d O'Clock" | /usr/bin/espeak --stdout | /usr/bin/aplay
