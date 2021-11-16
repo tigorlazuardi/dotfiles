4a04d6dac732671f014169b83c13019e2bb8077a
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-11-16 08:25:37 +0700

    feat: added more scripts

---
+++ _personal_shell/envs.sh
@@ -0,0 +1,2 @@
+# This should be set on /etc/profile.d folder for global settings
+export NVIM_LISTEN_ADDRESS=/tmp/nvimsocket
---
+++ kde-login-scripts/refresh-desktop.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sleep 5
+kquitapp5 plasmashell
+kstart5 plasmashell
