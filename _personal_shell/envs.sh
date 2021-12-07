87d84b96394e3ff84661a2eb3eb15f98d5e809db
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-12-07 10:34:31 +0700

    feat(personal_shell): added prettierd default config location file

--- _personal_shell/envs.sh
+++ _personal_shell/envs.sh
@@ -1,2 +1,5 @@
+#!/bin/sh
+
 # This should be set on /etc/profile.d folder for global settings
 export NVIM_LISTEN_ADDRESS=/tmp/nvimsocket
+export PRETTIERD_DEFAULT_CONFIG=$HOME/.config/nvim/linter-config/.prettierrc.toml
