a48cd1fb8242a07c9b49ff467c73377db35bf1bd
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2022-05-03 14:17:34 +0700

    feat(path): added NODE_PATH

--- _personal_shell/paths.sh
+++ _personal_shell/paths.sh
@@ -6,7 +6,8 @@ export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
 
 export ANDROID_SDK_ROOT='/opt/android-sdk'
-export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools/
-export PATH=$PATH:$ANDROID_SDK_ROOT/tools/bin/
-export PATH=$PATH:$ANDROID_ROOT/emulator
-export PATH=$PATH:$ANDROID_SDK_ROOT/tools/
+export PATH="$PATH:$ANDROID_SDK_ROOT/platform-tools/"
+export PATH="$PATH:$ANDROID_SDK_ROOT/tools/bin/"
+export PATH="$PATH:$ANDROID_ROOT/emulator"
+export PATH="$PATH:$ANDROID_SDK_ROOT/tools/"
+export NODE_PATH="$HOME/.local/npm/node_modules"
