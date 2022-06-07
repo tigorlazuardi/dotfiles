e3fda56aea3b5cb9fab5f2e8b8d96433827ee148
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2022-06-07 14:01:20 +0700

    feat(zsh): update plugins

--- README.md
+++ README.md
@@ -48,11 +48,11 @@ Recommended shells to symlink to is:
 
 > Features
 - update rsyncpi win
-- **alias:** mirror now take from indonesia and singapore
+- **alias:** added alias for psql "pgh"
 - **alias:** update alias for mirror
 - **alias:** watchexec now default to SIGNINT signal
 - **alias:** added dlo and dlm alias for ytmdl
-- **alias:** added alias for psql "pgh"
+- **alias:** mirror now take from indonesia and singapore
 - **env:** added CARGO_TARGET_DIR
 - **find-the-command:** used find the command loader
 - **gitconfig:** up
@@ -62,18 +62,19 @@ Recommended shells to symlink to is:
 - **ridit:** removed some subreddits
 - **rsync:** added alias for rsyncpi wsl
 - **rsyncpi:** change target ip
-- **rtorrent:** added rtorrent config
 - **rtorrent:** update config
+- **rtorrent:** added rtorrent config
 - **systemd:** update wireplumber
-- **tmux:** removed <c-b> prefix
 - **tmux:** f and c-f now used for choose-tree
+- **tmux:** removed <c-b> prefix
 - **vi-mode:** added vi mode
 - **yank:** added copy on mouse select
-- **ytmdl:** update config
 - **ytmdl:** added ytmdl config
+- **ytmdl:** update config
 - **zr:** moved plugin manager to zr
 - **zsh:** completion now case insensitive
 - **zsh:** added autocomplete on type
+- **zsh:** update plugins
 
 > Revert
 - **zsh:** removed autocomplete
--- _personal_shell/plugins.zsh
+++ _personal_shell/plugins.zsh
@@ -1,27 +1,44 @@
 #!/bin/zsh
-if ! command -v cargo &> /dev/null; then
+if ! command -v cargo &>/dev/null; then
 	curl https://sh.rustup.rs -sSf | sh -s -- -y
 fi
 
-if ! command -v zr &> /dev/null; then
-    cargo install zr
+if ! command -v zr &>/dev/null; then
+	cargo install zr
 fi
 
 if [[ ! -f ~/.config/zr.zsh ]] || [[ ~/.config/_personal_shell/plugins.zsh -nt ~/.config/zr.zsh ]]; then
-  zr \
-    junegunn/fzf.git/shell/key-bindings.zsh \
-	greymd/docker-zsh-completion \
-	zfben/zsh-npm \
-	Aloxaf/fzf-tab.git/fzf-tab.plugin.zsh \
-	ohmyzsh/ohmyzsh.git/plugins/git/git.plugin.zsh \
-	ohmyzsh/ohmyzsh.git/plugins/golang/golang.plugin.zsh \
-	MenkeTechnologies/zsh-cargo-completion \
-	zsh-users/zsh-autosuggestions \
-	jeffreytse/zsh-vi-mode \
-	zdharma-continuum/fast-syntax-highlighting \
-    > ~/.config/zr.zsh
-	# 	marlonrichert/zsh-autocomplete \
+	zr \
+		greymd/docker-zsh-completion \
+		zfben/zsh-npm \
+		Aloxaf/fzf-tab.git/fzf-tab.plugin.zsh \
+		ohmyzsh/ohmyzsh.git/plugins/git/git.plugin.zsh \
+		ohmyzsh/ohmyzsh.git/plugins/golang/golang.plugin.zsh \
+		MenkeTechnologies/zsh-cargo-completion \
+		zsh-users/zsh-autosuggestions \
+		jeffreytse/zsh-vi-mode \
+		romkatv/zsh-defer \
+		junegunn/fzf.git/shell/key-bindings.zsh \
+		marlonrichert/zsh-autocomplete \
+		zdharma-continuum/fast-syntax-highlighting \
+		>~/.config/zr.zsh
 fi
 
-
 source ~/.config/zr.zsh
+
+# autoloaded after zsh-vi-mode
+function zvm_after_init() {
+	if [ -f "/usr/share/fzf/key-bindings.zsh" ]; then
+		source /usr/share/fzf/key-bindings.zsh
+	fi
+
+	if [ -f "/usr/share/fzf/completion.zsh" ]; then
+		source /usr/share/fzf/completion.zsh
+	fi
+
+	if [ -f "$HOME/.cache/zr/marlonrichert/zsh-autocomplete/zsh-autocomplete.plugin.zsh" ]; then
+		source $HOME/.cache/zr/marlonrichert/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+	fi
+}
+
+alias zrc="rm $HOME/.config/zr.zsh; rm -rf $HOME/.cache/zr"
